d74ccfd8dd2170879b8ae2ae33a9e5b833ce0d8b
natnuo <natnu212@gmail.com>
2024-06-09 14:11:04 -0400

    djfksdh

--- tutorGPT/tutorGPT/settings.py
+++ tutorGPT/tutorGPT/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "project",
+    "django_extensions",
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
